1f0fb2ed23006aaf9a0e9188c69b0ae9b33bbcde
chanukyarajurs <chanukyarajurs@gmail.com>
2018-03-24 22:56:42 +0530

    Written Junit Tests from the JunitPlugin

---
+++ Demo3/src/test/java/com/chanu/Test.java
@@ -0,0 +1,72 @@
+package com.chanu;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void testSomeLibraryMethod() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@org.junit.Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
